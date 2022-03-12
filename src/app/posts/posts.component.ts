9bcaab3b5e3a991c952c1185330b8868d863f821
IvanHnilicka <ivan.hnilicka21@outlook.com>
2022-03-12 00:17:25 -0600

    Agrega vista Perfil y navegacion por rutas

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -24,6 +24,30 @@ export class PostsComponent implements OnInit {
     {
       "id": "92nrf72bvb",
       "imagen": "assets/imagenesPerfil/imagen2.jpg"
+    },
+    {
+      "id": "94jg824t",
+      "imagen": "assets/imagenesPerfil/imagen5.jpg"
+    },
+    {
+      "id": "hj9249t0g",
+      "imagen": "assets/imagenesPerfil/imagen6.jpg"
+    },
+    {
+      "id": "fqm39ngb",
+      "imagen": "assets/imagenesPerfil/imagen7.jpg"
+    },
+    {
+      "id": "01ng82nh3",
+      "imagen": "assets/imagenesPerfil/imagen8.jpg"
+    },
+    {
+      "id": "gm19344n1",
+      "imagen": "assets/imagenesPerfil/imagen9.jpg"
+    },
+    {
+      "id": "jh813ghw",
+      "imagen": "assets/imagenesPerfil/imagen10.jpg"
     }
   ];
 
